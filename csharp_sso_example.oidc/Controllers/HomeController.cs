5674c6a2cb6cbf5dca07c33344e5e1b830d213a3
Lin Thit Htoo <lth1212001@gmail.com>
2025-09-26 07:27:58 +0630

    -

--- csharp_sso_example.oidc/Controllers/HomeController.cs
+++ csharp_sso_example.oidc/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
