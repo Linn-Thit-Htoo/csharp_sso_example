8b0357a9a5cef9469e86dcb6f2a1248e6dea80f9
Lin Thit Htoo <lth1212001@gmail.com>
2025-09-28 00:54:06 +0630

    -

--- csharp_sso_example.api/Startup.cs
+++ csharp_sso_example.api/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Threading.Tasks;
 
 namespace csharp_sso_example.api;
 
