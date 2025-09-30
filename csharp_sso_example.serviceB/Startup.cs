91e80928003fb06871546525f956c27655b367db
Lin Thit Htoo <lth1212001@gmail.com>
2025-10-01 00:09:39 +0630

    -

--- csharp_sso_example.serviceB/Startup.cs
+++ csharp_sso_example.serviceB/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
